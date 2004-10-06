79e6a03d2693bdad58abc3c1f48c1c600c132ba8
qbix79 <qbix79@ed541006-0bf7-43e9-8c4d-6fc63c346d47>
2004-10-06 19:24:24 +0000

    hard to test php
    git-svn-id: https://svn.code.sf.net/p/dosbox/code-0/web/trunk@32 ed541006-0bf7-43e9-8c4d-6fc63c346d47

--- web/links.php
+++ web/links.php
@@ -23,7 +23,6 @@ template_pagebox_start("DOSBox related links", 900);
 			<font face="Verdana, Arial, Helvetica, sans-serif" size="2">
 			<a href="http://www.mypixels.dk/forums/" target="_blank">www.mypixels.dk</a> - A forum which discusses the CVS version of DOSBox.
 			</font></li>						
-			<li>
 			</ul>';
 template_pagebox_end();
 										    
