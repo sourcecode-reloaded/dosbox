a5e15221c94d4cb726913bfade6e36bebb6cdc6f
qbix79 <qbix79@ed541006-0bf7-43e9-8c4d-6fc63c346d47>
2006-01-24 15:13:47 +0000

    typo
    git-svn-id: https://svn.code.sf.net/p/dosbox/code-0/web/trunk@42 ed541006-0bf7-43e9-8c4d-6fc63c346d47

--- web/links.php
+++ web/links.php
@@ -37,7 +37,7 @@ template_pagebox_start("General Emulation links", 900);
 			<a href="http://www.retrogames.com/" target="_blank">Retrogames</a> - Retrogames, your one stop emulation site. That should explain it enough.
 			</font></li>
 			<li><font face="Verdana, Arial, Helvetica, sans-serif" size="2">
-			<a href="http://www.emu-france.com/" target="_blank">Emu France</a> - a good french source for your daily emulation news.
+			<a href="http://www.emu-france.com/" target="_blank">Emu-France</a> - a good french source for your daily emulation news.
 			</font></li>
 			</ul>';
 template_pagebox_end();
