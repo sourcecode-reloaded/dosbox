d22cd53c234ab0abf101e6d64f640a68ffc30735
qbix79 <qbix79@ed541006-0bf7-43e9-8c4d-6fc63c346d47>
2009-06-15 20:12:38 +0000

    Status.php updates
    git-svn-id: https://svn.code.sf.net/p/dosbox/code-0/web/trunk@53 ed541006-0bf7-43e9-8c4d-6fc63c346d47

--- web/status.php
+++ web/status.php
@@ -2,37 +2,37 @@
 // this src is written under the terms of the GPL-licence, see gpl.txt for futher details
 	include("include/standard.inc.php");
 	sstart();
-if ($user['priv']['status_manage']==1)
+if (isset($user) && $user['priv']['status_manage']==1)
 {
-	if ($_GET['changing']==1)
+	if (isset($_POST['updateID'],$_POST['name'],$_POST['status']) && $_GET['changing']==1)
 	{
 
 		
 		if (is_numeric($_POST['status']) AND $_POST['status'] < 101 AND $_POST['name'] != '' AND $_POST['note'] != '')
 		{
 			
-			$updateID	= mysql_escape_string(stripslashes($_POST['updateID']));
-			$name 		= mysql_escape_string(stripslashes($_POST['name']));
-			$status 	= mysql_escape_string(stripslashes($_POST['status']));
-			$note 		= mysql_escape_string(stripslashes($_POST['note']));
+			$updateID	= mysql_real_escape_string(stripslashes($_POST['updateID']));
+			$name 		= mysql_real_escape_string(stripslashes($_POST['name']));
+			$status 	= mysql_real_escape_string(stripslashes($_POST['status']));
+			$note 		= mysql_real_escape_string(stripslashes($_POST['note']));
 			
 			mysql_query("UPDATE status_items SET name='$name', percent=$status, note='$note' WHERE ID=$updateID");
 			
-			Header("Location: status.php?changeID=".$_GET['catID']);
+			Header("Location: status.php?changeID=".intval($_GET['catID']));
 		}
 		else
-			Header("Location: status.php?problem=1&changeID=".$_GET['catID']);
+			Header("Location: status.php?problem=1&changeID=".intval($_GET['catID']));
 
 	}
 
-	if ($_GET['adding'])
+	if (isset($_GET['catID'],$_POST['name'],$_POST['status']) && $_GET['adding'])
 	{
 		if (is_numeric($_POST['status']) AND $_POST['status'] < 101 AND $_POST['name'] != '' AND $_POST['note'] != '')
 		{
-			$catID		= mysql_escape_string(stripslashes($_GET['catID']));
-			$name 		= mysql_escape_string(stripslashes($_POST['name']));
-			$status 	= mysql_escape_string(stripslashes($_POST['status']));
-			$note 		= mysql_escape_string(stripslashes($_POST['note']));
+			$catID		= mysql_real_escape_string(stripslashes($_GET['catID']));
+			$name 		= mysql_real_escape_string(stripslashes($_POST['name']));
+			$status 	= mysql_real_escape_string(stripslashes($_POST['status']));
+			$note 		= mysql_real_escape_string(stripslashes($_POST['note']));
 			
 			mysql_query("
 					INSERT INTO 
@@ -40,12 +40,12 @@ if ($user['priv']['status_manage']==1)
 					VALUES ($catID, '$name', $status, '$note')
 				");
 		
-			Header("Location: status.php?changeID=".$_GET['catID']);
+			Header("Location: status.php?changeID=".intval($_GET['catID']));
 		}
 		else
-			Header("Location: status.php?problem=1&changeID=".$_GET['catID']);		
+			Header("Location: status.php?problem=1&changeID=".intval($_GET['catID']));		
 	}
-	if ($_GET['removeID'])
+	if (isset($_GET['removeID']))
 	{
 		$removeID = mysql_escape_string(stripslashes($_GET['removeID']));
 		$catID = mysql_escape_string(stripslashes($_GET['catID']));
@@ -55,29 +55,29 @@ if ($user['priv']['status_manage']==1)
 		Header("Location: status.php?changeID=".$catID);
 
 	}
-	if ($_GET['changeID'])
+	if (isset($_GET['changeID']) &&is_numeric($_GET['changeID']))
 	{
 		template_header();
 		echo '<br><table width="100%"><tr><td width="14">&nbsp;</td><td>';// start of framespacing-table		
 		
 		template_pagebox_start("Changing DOSBox development status", 890);			
 		
-		echo '<font face="Verdana, Arial, Helvetica, sans-serif" size="2"><br>';
+		echo '<br>';
 		
 		
 		if ($_GET['problem']==1)
 			echo '<b>Something went wrong, please try again!</b><br>';
 		
 		echo '<a href="status.php?show_status=1">Click here</a> to get back to the status page!<br><br><br>';
-		status_change($_GET['changeID']);
+		status_change(intval($_GET['changeID']));
 				
-			echo '<br><br></font>
+			echo '<br><br>
 			
-			<font face="Verdana, Arial, Helvetica, sans-serif" size="2"><b>Add new item</b><font size="2"><br>
+			<strong>Add new item</strong><br>
 			
 
 			<table cellspacing="0" cellpadding="0">
-			<form action="status.php?adding=1&catID='.$_GET['changeID'].'" method="POST" name="adding_statusID"><tr><td>
+			<form action="status.php?adding=1&catID='.intval($_GET['changeID']).'" method="POST" name="adding_statusID"><tr><td>
 				<input type="text" value="'.$result[0].'" name="name" maxlength="40" size="25">
 			</td>
 
@@ -112,15 +112,14 @@ if ($user['priv']['status_manage']==1)
 
 	}
 }
-if ($_GET['show_status']==1)
+if (isset($_GET['show_status']) && $_GET['show_status']==1)
 {	
 	template_header();
 	
 	echo '<br><table width="100%"><tr><td width="14">&nbsp;</td><td>';// start of framespacing-table
-		echo '<font face="Verdana, Arial, Helvetica, sans-serif" size="2">
-		This shows a basic overview of the different systems emulated in DOSBox.<br><br></font>';
+		echo 'This shows a basic overview of the different systems emulated in DOSBox.<br><br>';
 				
-	show_status_db($user['priv']['status_manage']);
+	show_status_db(isset($user)?$user['priv']['status_manage']:0);
 	
 		
 	echo '<br>';	
